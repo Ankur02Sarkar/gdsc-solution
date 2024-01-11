921b643675ee0e1a65cd69713afdb80871dcf0a8
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-01-11 15:03:24 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Hero from './components/Hero'
+import NewestDeals from './components/NewestDeals'
+import AboutUs from './components/AboutUs'
+import BestDeals from './components/BestDeals'
+import Footer from './components/Footer'
 
 export default function App() {
-  const [data, setData] = useState("");
-  const getData = async () => {
-    const res = await axios.get("/api/getData");
-    setData(res.data);
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-  return <div>{data}</div>;
+
+  return (
+    <>
+    <div className="App  w-11/12 md:w-4/5 m-auto">
+      <Hero/>
+      <NewestDeals/>
+      <AboutUs/>
+      <BestDeals/>
+    </div>
+      <Footer/>
+    </>
+  )
 }
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+})
